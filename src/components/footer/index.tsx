eafdef7f7026c077a7bb38bb77769251219f3448
bmalkes <bmalkes@gmail.com>
2024-02-28 21:20:14 +0200

    feat:add new testimonial

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -50,9 +50,7 @@ const Reccomend = () => {
   return (
     <section className=" h-full " id="contact">
       <img src={vetor2} alt="background vetor" className="w-full" />
-      <h2 className="text-6xl font-bold mt-12 text-center">
-        המלצות מלקוחותינו
-      </h2>
+      <h2 className="text-6xl font-bold mt-12 text-center">המלצות עלינו</h2>
       <div>
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
