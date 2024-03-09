a7ea52688ea18639dcb05457dfa45b5a5a599b1d
bmalkes <bmalkes@gmail.com>
2024-03-09 20:41:18 +0200

    fix:css change

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -54,7 +54,7 @@ const Hero = () => {
           <img
             src={hand}
             alt=""
-            className="lg:h-full lg:object-center object-cover object-left overflow-visible w-full h-[450px] md:h-[800px]"
+            className="lg:h-full lg:object-center object-cover object-left-bottom overflow-visible w-full h-full md:h-[800px]"
           />
         </div>
       </div>
