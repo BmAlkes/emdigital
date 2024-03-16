bb2badd910a2df2d149435f95cff137fa13b6539
bmalkes <bmalkes@gmail.com>
2024-03-16 13:39:31 +0200

    feat:add loader

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,7 +1,11 @@
+import logo from "../../assets/svg/Logo3.svg";
+
 const Loader = () => {
   return (
     <div className="loader h-screen w-full bg-black absolute top-0 z-50">
-      <div className="intern"></div>
+      <div className="intern">
+        <img src={logo} alt="logo site" className="w-[200px]" />
+      </div>
       <div className="external-shadow">
         <div className="central"></div>
       </div>
